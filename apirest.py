afbca269e60d3f105647a2f898e118a7dd95c6f2
Tupi Guedes <tupiguedes@outlook.com>
2022-10-16 18:56:07 -0300

    Mudando a estrutura de pastas do projeto

--- apirest.py
+++
@@ -1,7 +0,0 @@
-from api import app
-import os
-
-if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
-    app.run('0.0.0.0', port=port)
-    # app.run('0.0.0.0', port=port, debug=True)
--- testapi.py
+++
@@ -1,5 +0,0 @@
-from requests import get
-from json import loads
-
-api = get('http://127.0.0.1:5000/mostrar_tudo')
-print(loads(api.content))
